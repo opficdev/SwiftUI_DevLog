e7659023169f52e19a1db1511029e61560b0b352
opfic <opficdev@gmail.com>
2025-05-07 17:01:58 +0900

    feat: 현재 선택된 테마가 어떤 테마인지 보여줄 수 있도록 추가

--- DevLog/View/Setting/SettingView.swift
+++ DevLog/View/Setting/SettingView.swift
@@ -8,18 +8,27 @@
 import SwiftUI
 
 struct SettingView: View {
+    @AppStorage("theme") var theme: SystemTheme = .automatic
+    @StateObject private var settingVM = SettingViewModel()
+    
     var body: some View {
         List {
             Section {
-                NavigationLink(destination: ThemeView()) {
+                NavigationLink(destination: ThemeView().environmentObject(settingVM)) {
                     HStack {
                         Text("테마")
                             .foregroundStyle(Color.primary)
                         Spacer()
+                        Text("\(settingVM.theme)")
+                            .foregroundStyle(Color.gray)
+                    }
+                    .onAppear {
+                        settingVM.theme = theme.localizedName
                     }
                 }
             }
         }
+        .navigationTitle("설정")
     }
 }
 
