b4ac366984351f75c13dc662121f70a4e1cd95c7
opfic <opficdev@gmail.com>
2025-05-07 16:54:53 +0900

    refactor: 언어 현지화를 위해 localizedName 추가

--- DevLog/Enum/SystemTheme.swift
+++ DevLog/Enum/SystemTheme.swift
@@ -7,13 +7,24 @@
 
 import SwiftUI
 
-enum SystemTheme: String, Identifiable {
+enum SystemTheme: Int, Identifiable {
     case automatic
     case light
     case dark
     
-    var id: String {
+    var id: Int {
         rawValue
+    }
+    
+    var localizedName: String {
+        switch self {
+        case .automatic:
+            return NSLocalizedString("자동", comment: "automatic")
+        case .light:
+            return NSLocalizedString("라이트 모드", comment: "light")
+        case .dark:
+            return NSLocalizedString("다크 모드", comment: "dark")
+        }
     }
     
     var colorScheme: ColorScheme? {
---
+++ DevLog/ViewModel/ProfileViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileViewModel.swift
+//  DevLog
+//
+//  Created by opfic on 5/7/25.
+//
+
+import Foundation
