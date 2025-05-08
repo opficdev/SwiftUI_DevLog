34bdb3489ac9f2f89aabf06d0fe72bcdb6f2529e
opfic <opficdev@gmail.com>
2025-05-08 10:18:54 +0900

    refactor: Google Sign in 추가

--- DevLog/Delegate/AppDelegate.swift
+++ DevLog/Delegate/AppDelegate.swift
@@ -5,12 +5,27 @@
 //  Created by opfic on 5/7/25.
 //
 
-import SwiftUI
+import UIKit
 import FirebaseCore
+import FirebaseAuth
+import GoogleSignIn
 
-class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
-    return true
-  }
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        return GIDSignIn.sharedInstance.handle(url)
+    }
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+
+    //  gRPC 관련 환경 변수 설정 (GRPC_TRACE 제거)
+    setenv("GRPC_VERBOSITY", "ERROR", 1)
+    unsetenv("GRPC_TRACE") // GRPC_TRACE 환경 변수 제거
+
+    //  Firebase 디버그 로그 활성화
+    //  FirebaseConfiguration.shared.setLoggerLevel(.debug)
+
+        return true
+    }
 }
