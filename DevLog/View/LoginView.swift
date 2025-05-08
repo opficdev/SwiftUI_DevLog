5e907707be2c121410ac262aa668380df8ce46b8
opfic <opficdev@gmail.com>
2025-05-08 15:48:41 +0900

    feat: 구글,깃헙,애플 소셜 로그인을 할 수 있도록 구현한 로그인뷰

---
+++ DevLog/View/LoginView.swift
@@ -0,0 +1,83 @@
+//
+//  LoginView.swift
+//  SnapPlan
+//
+//  Created by opfic on 12/30/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    @EnvironmentObject private var firebaseVM: FirebaseViewModel
+    @Environment(\.colorScheme) var colorScheme
+    
+    let screenWidth = UIScreen.main.bounds.width
+    
+    var body: some View {
+        ZStack {
+            VStack {
+                Spacer()
+                Group {
+//                    if colorScheme == .light {
+                        Image("DevLog")
+                            .resizable()
+//                    }
+//                    else {
+//                        Image("dark_logo")
+//                            .resizable()
+//                    }
+                }
+                .scaledToFit()
+                .frame(width: screenWidth / 5)
+                Spacer()
+                VStack(spacing: 20) {
+                    LoginButton(logo: Image("Google"), text: "구글 계정으로 로그인") {
+                        if firebaseVM.isConnected {
+                            Task {
+                                try await firebaseVM.signInGoogle()
+                            }
+                        }
+                        else {
+                            firebaseVM.showNetworkAlert = true
+                        }
+                    }
+                    .frame(width: screenWidth * 3 / 4, height: screenWidth / 10)
+                    
+                    LoginButton(logo: Image("Github"), text: "깃헙 계정으로 로그인") {
+                        if firebaseVM.isConnected {
+                            Task {
+                                
+                            }
+                        }
+                        else {
+                            firebaseVM.showNetworkAlert = true
+                        }
+                    }
+                    .frame(width: screenWidth * 3 / 4, height: screenWidth / 10)
+                        
+                    LoginButton(logo: Image("Apple"), text: "애플 계정으로 로그인") {
+                        if firebaseVM.isConnected {
+                            Task {
+                                try await firebaseVM.signInApple()
+                            }
+                        }
+                        else {
+                            firebaseVM.showNetworkAlert = true
+                        }
+                    }
+                    .frame(width: screenWidth * 3 / 4, height: screenWidth / 10)
+                }
+                .padding(.bottom, 30)
+                Text("로그인하면 사용 약관 및 개인 정보 취급 방침에 동의하게 됩니다.")
+                    .font(.caption2)
+                    .foregroundStyle(Color.gray)
+                    .multilineTextAlignment(.center)
+                    .padding(.vertical)
+            }
+        }
+    }
+}
+
+#Preview {
+    LoginView()
+}
