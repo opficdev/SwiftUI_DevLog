0c21d71381a1e91a63675dfea5618aeb044e73cc
opfic <opficdev@gmail.com>
2025-05-09 23:48:58 +0900

    refactor: 앱 아이콘, 앱 푸시알림, 로그아웃 기능을 구현

--- DevLog/View/Setting/SettingView.swift
+++ DevLog/View/Setting/SettingView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct SettingView: View {
     @AppStorage("theme") var theme: SystemTheme = .automatic
     @StateObject private var settingVM = SettingViewModel()
+    @EnvironmentObject private var firebaseVM: FirebaseViewModel
     
     var body: some View {
         List {
@@ -26,9 +27,54 @@ struct SettingView: View {
                         settingVM.theme = theme.localizedName
                     }
                 }
+                NavigationLink(destination: AppIconView()) {
+                    HStack {
+                        Text("앱 아이콘")
+                            .foregroundStyle(Color.primary)
+                        Spacer()
+                        Text(settingVM.appIcon)
+                            .foregroundStyle(Color.gray)
+                    }
+                    .onAppear {
+                        
+                    }
+                }
+                NavigationLink(destination: AlertView()) {
+                    HStack {
+                        Text("알림")
+                            .foregroundStyle(Color.primary)
+                        Spacer()
+                        Text(settingVM.notification)
+                            .foregroundStyle(Color.gray)
+                    }
+                }
+            }
+            
+            Section {
+                Button(role: .destructive, action: {
+                    settingVM.logoutAlert = true
+                }) {
+                    Text("로그아웃")
+                }
             }
         }
         .navigationTitle("설정")
+        .alert("로그아웃", isPresented: $settingVM.logoutAlert) {
+            Button(role: .cancel, action: {
+                settingVM.logoutAlert = false
+            }) {
+                Text("취소")
+            }
+            Button(role: .destructive, action: {
+                Task {
+                    try await firebaseVM.signOut()
+                }
+            }) {
+                Text("확인")
+            }
+        } message: {
+            Text("로그아웃하시겠습니까?")
+        }
     }
 }
 
