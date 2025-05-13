8d1be749b3198329028b2297ca0b7cdb4f845e03
opfic <opficdev@gmail.com>
2025-05-13 17:19:17 +0900

    feat: 사용자의 프로필을 확인 및 수정할 수 있는 뷰

---
+++ DevLog/View/Tab/ProfileView.swift
@@ -0,0 +1,113 @@
+//
+//  ProfileView.swift
+//  DevLog
+//
+//  Created by opfic on 5/7/25.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    @EnvironmentObject private var firebaseVM: FirebaseViewModel
+    @FocusState private var focusedOnStatusMsg: Bool
+    @State private var showDoneBtn: Bool = false
+    
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                VStack(alignment: .leading, spacing: 16) {
+                    HStack {
+                        firebaseVM.avatar
+                            .scaledToFill()
+                            .frame(width: 60, height: 60)
+                            .cornerRadius(30)
+                            .foregroundStyle(Color(UIColor.systemGray4))
+                        
+                        VStack(alignment: .leading) {
+                            Text(firebaseVM.name)
+                                .font(.title2)
+                                .bold()
+                            Text(firebaseVM.email)
+                                .font(.caption2)
+                                .foregroundStyle(Color.gray)
+                        }
+                    }
+                    
+                    HStack {
+                        HStack {
+                            Image(systemName: "face.smiling")
+                            
+                            TextField(text: $firebaseVM.statusMsg) {
+                                HStack {
+                                    Text("상태 설정")
+                                }
+                            }
+                            .focused($focusedOnStatusMsg)
+                            
+                            if !firebaseVM.statusMsg.isEmpty && showDoneBtn {
+                                Button(action: {
+                                    firebaseVM.statusMsg = ""
+                                }) {
+                                    Image(systemName: "xmark.circle.fill")
+                                }
+                                .transition(.move(edge: .trailing).combined(with: .opacity))
+                            }
+                        }
+                        .foregroundStyle(Color.gray)
+                        .padding(8)
+                        .background(
+                            RoundedRectangle(cornerRadius: 10)
+                                .fill(Color(UIColor.systemGray5))
+                        )
+                        if showDoneBtn {
+                            Button(action: {
+                                focusedOnStatusMsg = false
+                                Task {
+                                    do {
+                                        try await firebaseVM.upsertStatusMsg()
+                                    } catch {
+                                        
+                                    }
+                                }
+                            }) {
+                                Text("완료")
+                            }
+                            .transition(.move(edge: .trailing).combined(with: .opacity))
+                        }
+                    }
+                }
+                .padding(.horizontal)
+            }
+            .frame(maxWidth: .infinity)
+            .background(Color(UIColor.systemGroupedBackground))
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    HStack(spacing: 0) {
+                        NavigationLink(destination: SettingView()) {
+                            Image(systemName: "gearshape")
+                        }
+                        Button(action: {
+                            
+                        }) {
+                            Image(systemName: "plus")
+                        }
+                    }
+                }
+            }
+            .onAppear {
+                Task {
+                    do {
+                        try await firebaseVM.fetchStatusMsg()
+                    } catch {
+                        
+                    }
+                }
+            }
+            .onChange(of: focusedOnStatusMsg) { newValue in
+                withAnimation {
+                    showDoneBtn = newValue
+                }
+            }
+        }
+    }
+}
