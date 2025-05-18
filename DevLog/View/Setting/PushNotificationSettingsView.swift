078fb0b2e404f879ba7caae1d03b23ac92e0ae54
opfic <opficdev@gmail.com>
2025-05-18 22:32:24 +0900

    update

--- DevLog/View/Setting/PushNotificationSettingsView.swift
+++ DevLog/View/Setting/PushNotificationSettingsView.swift
@@ -1,18 +1,18 @@
 //
-//  LoadingView.swift
+//  PushNotificationSettingsView.swift
 //  DevLog
 //
-//  Created by opfic on 5/16/25.
+//  Created by opfic on 5/14/25.
 //
 
 import SwiftUI
 
-struct LoadingView: View {
+struct PushNotificationSettingsView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    LoadingView()
+    PushNotificationSettingsView()
 }
