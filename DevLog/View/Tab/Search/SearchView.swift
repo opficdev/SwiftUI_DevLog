e6410794d44eaf184b1421d6e834f37ff3edbe13
opfic <opficdev@gmail.com>
2025-05-22 16:52:53 +0900

    refactor: isSearching 프로퍼티 래퍼를 사용하고 나머지 뷰는 SearchView에서 구현하도록 수정

--- DevLog/View/Tab/Search/SearchView.swift
+++ DevLog/View/Tab/Search/SearchView.swift
@@ -8,13 +8,37 @@
 import SwiftUI
 
 struct SearchView: View {
+    @Environment(\.dismiss) private var dismiss
     @State private var searchText: String = ""
+    @State private var isFocused: Bool = false
     
     var body: some View {
         NavigationStack {
-            SearchedView(searchText: $searchText)
+            SearchedView(searchText: $searchText, focused: $isFocused)
                 .searchable(text: $searchText, prompt: "DevLog 검색")
+            GeometryReader { geometry in
+                ScrollView {
+                    VStack{
+                        if isFocused {
+                            Divider()
+                            if searchText.isEmpty {
+                                Spacer()
+                                Text("앱 내 컨텐츠나 개발자 문서를 검색할 수 있어요.")
+                                Spacer()
+                            }
+                            else {
+                                Text("검색 내용이 보여지는 곳")
+                            }
+                        }
+                        else {
+                            
+                        }
+                    }
+                    .frame(width: geometry.size.width)
+                    .frame(minHeight: isFocused && searchText.isEmpty ? geometry.size.height : 0)
+                }
                 .navigationTitle("검색")
+            }
         }
     }
 }
--- DevLog/View/Tab/Search/SearchedView.swift
+++ DevLog/View/Tab/Search/SearchedView.swift
@@ -10,30 +10,12 @@ import SwiftUI
 struct SearchedView: View {
     @Environment(\.isSearching) private var isSearching
     @Binding var searchText: String
+    @Binding var focused: Bool
     
     var body: some View {
-        GeometryReader { geometry in
-            ScrollView {
-                VStack{
-                    if isSearching {
-                        Divider()
-                        if searchText.isEmpty {
-                            Spacer()
-                            Text("앱 내 컨텐츠나 개발자 문서를 검색할 수 있어요.")
-                            Spacer()
-                        }
-                        else {
-                            Text("검색 내용이 보여지는 곳")
-                        }
-                    }
-                    else {
-                        Text("기본적으로 개발자 문서가 보여지는 곳")
-                    }
-                }
-                .frame(width: geometry.size.width)
-                .frame(minHeight: isSearching && searchText.isEmpty ? geometry.size.height : 0)
-            }
+        EmptyView()
+        .onChange(of: isSearching) { newValue in
+            focused = newValue
         }
-        
     }
 }
