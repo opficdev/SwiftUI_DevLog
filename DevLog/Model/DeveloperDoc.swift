08bc98bde54013f38ac0aae47d2730832ef92eac
opfic <opficdev@gmail.com>
2025-05-24 21:06:56 +0900

    refactor: fetch 메서드를 호출하여 title 및 웹 이미지를 가져와서 구조체를 생성할 수 있도록 추가

--- DevLog/Model/DeveloperDoc.swift
+++ DevLog/Model/DeveloperDoc.swift
@@ -6,16 +6,69 @@
 //
 
 import SwiftUI
+import LinkPresentation
+import UniformTypeIdentifiers
 
 struct DeveloperDoc: Identifiable {
     let id = UUID()
     var image: UIImage?
     var title: String
     var urlString: String
-    
-    init(_ title: String, urlString: String, uiImage: UIImage? = nil) {
+
+    init(image: UIImage?, title: String, urlString: String) {
+        self.image = image
         self.title = title
         self.urlString = urlString
-        self.image = uiImage
+    }
+
+    static func fetch(from urlString: String) async throws -> DeveloperDoc {
+        guard let url = URL(string: urlString) else {
+            throw URLError(.badURL)
+        }
+        
+        return await fetch(from: url)
+    }
+
+    static func fetch(from url: URL) async -> DeveloperDoc {
+        let provider = LPMetadataProvider()
+        var image: UIImage? = nil
+        var title: String = ""
+        var urlString: String = url.absoluteString
+
+        do {
+            let metadata = try await provider.startFetchingMetadata(for: url)
+            image = try await convertToImage(metadata)
+            title = metadata.title ?? ""
+            urlString = metadata.url?.host() ?? url.absoluteString
+        } catch {
+            print("Error fetching metadata: \(error.localizedDescription)")
+        }
+
+        return DeveloperDoc(image: image, title: title, urlString: urlString)
+    }
+
+    static func convertToImage(_ metaData: LPLinkMetadata) async throws -> UIImage? {
+        let imageType = UTType.image.identifier
+
+        if let imageProvider = metaData.imageProvider,
+            imageProvider.hasItemConformingToTypeIdentifier(imageType) {
+            let imageItem = try await imageProvider.loadItem(forTypeIdentifier: imageType)
+
+            switch imageItem {
+            case let uiImage as UIImage:
+                return uiImage
+            case let url as URL:
+                if let data = try? Data(contentsOf: url) {
+                    return UIImage(data: data)
+                }
+            case let data as Data:
+                return UIImage(data: data)
+            case let nsData as NSData:
+                return UIImage(data: nsData as Data)
+            default:
+                return nil
+            }
+        }
+        return nil
     }
 }
