ee6895aa4196185631056fcb0f8eef212b8402ca
opfic <opficdev@gmail.com>
2025-05-29 09:57:59 +0900

    fix: iOS 18 미만에서의 navigationTitle 영억 이슈 해결

--- DevLog/View/Tab/Search/SearchView.swift
+++ DevLog/View/Tab/Search/SearchView.swift
@@ -28,6 +28,7 @@ struct SearchView: View {
                 )) {
                     if let url = selectedDoc?.url {
                         WebView(url: url)
+                            .navigationBarTitleDisplayMode(.inline) //  이렇게 명시해주지 않으면 iOS 18 미만에서는 Large 크기만큼의 상단의 영역을 어느정도 먹고있음
                     }
                 }
             GeometryReader { geometry in
