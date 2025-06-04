5987ece3c4ec397129cc8235764a5bc7f6b36ecf
opfic <opficdev@gmail.com>
2025-06-04 22:32:57 +0900

    feat: 구글 로그인, 인증에 필요한 비즈니스 로직이 구현된 서비스

---
+++ DevLog/Service/SocialLogin/GoogleSignInService.swift
@@ -0,0 +1,100 @@
+//
+//  GoogleSignInService.swift
+//  DevLog
+//
+//  Created by opfic on 6/4/25.
+//
+
+import FirebaseAuth
+import FirebaseFirestore
+import Foundation
+import GoogleSignIn
+
+class GoogleSignInService {
+    private let db = Firestore.firestore()
+    
+    private var user: User? { Auth.auth().currentUser }
+    private var userId: String? { user?.uid }
+    private var userEmail: String? { user?.email }
+    
+    func signInWithGoogle() async throws -> User {
+        guard let topVC = topViewController() else {
+            throw URLError(.cannotFindHost)
+        }
+        
+        let gidSignIn = try await GIDSignIn.sharedInstance.signIn(withPresenting: topVC)
+        
+        guard let idToken = gidSignIn.user.idToken?.tokenString else {
+            throw URLError(.badServerResponse)
+        }
+        
+        let accessToken = gidSignIn.user.accessToken.tokenString
+        let credential = GoogleAuthProvider.credential(withIDToken: idToken, accessToken: accessToken)
+        
+        let result = try await Auth.auth().signIn(with: credential)
+        
+        if let photoURL = gidSignIn.user.profile?.imageURL(withDimension: 200) {
+            let changeRequest = result.user.createProfileChangeRequest()
+            changeRequest.photoURL = photoURL
+            changeRequest.displayName = gidSignIn.user.profile?.name
+            
+            try await changeRequest.commitChanges()
+        }
+        
+        return result.user
+    }
+    
+    func linkWithGoogle() async throws {
+        guard let user = Auth.auth().currentUser, let topVC = topViewController() else {
+            throw URLError(.userAuthenticationRequired)
+        }
+        
+        if GIDSignIn.sharedInstance.hasPreviousSignIn() {
+            GIDSignIn.sharedInstance.signOut()
+        }
+        
+        let gidSignIn = try await GIDSignIn.sharedInstance.signIn(withPresenting: topVC)
+        
+        guard let googleEmail = gidSignIn.user.profile?.email else {
+            throw EmailFetchError.emailNotFound
+        }
+        
+        if googleEmail != self.userEmail {
+            throw EmailFetchError.emailMismatch
+        }
+        
+        guard let idToken = gidSignIn.user.idToken?.tokenString else {
+            throw URLError(.badServerResponse)
+        }
+        
+        let accessToken = gidSignIn.user.accessToken.tokenString
+        let credential = GoogleAuthProvider.credential(withIDToken: idToken, accessToken: accessToken)
+        
+        try await user.link(with: credential)
+    }
+}
+
+extension GoogleSignInService {
+    func topViewController(controller: UIViewController? = nil) -> UIViewController? {
+        let keyWindow = UIApplication.shared.connectedScenes
+            .compactMap { $0 as? UIWindowScene }
+            .flatMap { $0.windows }
+            .first { $0.isKeyWindow }
+
+        let controller = controller ?? keyWindow?.rootViewController
+        
+        if let navigationController = controller as? UINavigationController {
+            return topViewController(controller: navigationController.visibleViewController)
+        }
+        
+        if let tabController = controller as? UITabBarController, let selected = tabController.selectedViewController {
+            return topViewController(controller: selected)
+        }
+        
+        if let presented = controller?.presentedViewController {
+            return topViewController(controller: presented)
+        }
+        
+        return controller
+    }
+}
