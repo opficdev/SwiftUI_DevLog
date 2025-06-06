706ceb5fd1e0a7faa89425102968f1a03f808851
opfic <opficdev@gmail.com>
2025-06-06 19:00:51 +0900

    feat: SettingView에 사용되는 뷰모델

--- DevLog/ViewModel/SettingViewModel.swift
+++ DevLog/ViewModel/SettingViewModel.swift
@@ -8,13 +8,45 @@
 import SwiftUI
 import UIKit
 
+@MainActor
 class SettingViewModel: ObservableObject {
-    @Published var signOutAlert = false
-    @Published var deleteUserAlert = false
+    private let authSvc: AuthService
+    private let networkSvc: NetworkActivityService
+    private let userSvc: UserService
     @Published var theme: String = ""
     @Published var appIcon: String = ""
     
-    @MainActor
+    @Published var signIn: Bool? = nil
+    @Published var deleteUserAlert: Bool = false
+    @Published var signOutAlert: Bool = false
+    
+    // NetworkActivityService와 연결되는 Published 프로퍼티
+    @Published var isConnected: Bool = true
+    @Published var isLoading: Bool = false
+    @Published var showNetworkAlert: Bool = false
+
+    
+    init(authSvc: AuthService, networkSvc: NetworkActivityService, userSvc: UserService) {
+        self.authSvc = authSvc
+        self.networkSvc = networkSvc
+        self.userSvc = userSvc
+    
+        // self.isLoading을 network.isLoading와 단방향 연결
+        self.$isLoading
+            .receive(on: DispatchQueue.main)
+            .assign(to: &self.networkSvc.$isLoading)
+        
+        // NetworkActivityService.isConnected를 self.isConnected와와 단방향 연결
+        self.networkSvc.$isConnected
+            .receive(on: DispatchQueue.main)
+            .assign(to: &self.$isConnected)
+        
+        // NetworkActivityService.showNetworkAlert를 self.showNetworkAlert와 단방향 연결
+        self.networkSvc.$showNetworkAlert
+            .receive(on: DispatchQueue.main)
+            .assign(to: &self.$showNetworkAlert)
+    }
+    
     func setAppIcon(iconName: String? = nil) async throws {
         try await withCheckedThrowingContinuation { (continuation: CheckedContinuation<Void, Error>) in
             UIApplication.shared.setAlternateIconName(iconName) { error in
@@ -25,6 +57,42 @@ class SettingViewModel: ObservableObject {
                     continuation.resume()
                 }
             }
+        }
+    }
+    
+    func signOut() async {
+        do {
+            self.isLoading = true
+            defer {
+                self.isLoading = false
+            }
+            guard let user = self.authSvc.user else { throw URLError(.userAuthenticationRequired) }
+            
+            try await self.authSvc.signOut(user: user)
+            
+            // AppStorage 전체를 삭제하는 코드
+            UserDefaults.standard.removePersistentDomain(forName: Bundle.main.bundleIdentifier!)
+        } catch {
+            print("Error signing out: \(error.localizedDescription)")
+            self.signOutAlert = true
+        }
+    }
+    
+    func deleteUser() async {
+        do {
+            self.isLoading = true
+            defer {
+                self.isLoading = false
+            }
+            
+            guard let user = self.authSvc.user else { throw URLError(.userAuthenticationRequired) }
+            
+            try await self.userSvc.deleteUser(user: user)
+            
+            UserDefaults.standard.removePersistentDomain(forName: Bundle.main.bundleIdentifier!)
+        } catch {
+            print("Error deleting user: \(error.localizedDescription)")
+            self.deleteUserAlert = true
         }
     }
 }
