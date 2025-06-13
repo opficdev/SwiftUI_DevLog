a41d9237173d264db2f95b1601429763979aceda
opfic <opficdev@gmail.com>
2025-06-13 17:17:10 +0900

    feat: TODO의 내용 일부를 볼 수 있는 기능 추가

--- DevLog/View/Tab/Home/Todo/TodoView.swift
+++ DevLog/View/Tab/Home/Todo/TodoView.swift
@@ -9,7 +9,6 @@ import SwiftUI
 
 struct TodoView: View {
     @StateObject private var todoVM: TodoViewModel
-    @State private var searchText: String = ""
     @State private var showIssueFullScreen: Bool = false
     
     init(todoVM: TodoViewModel) {
@@ -18,93 +17,108 @@ struct TodoView: View {
     
     var body: some View {
         NavigationStack {
-            GeometryReader { geometry in
-                ScrollView {
-                    LazyVStack(spacing: 0) {
-                        if todoVM.todos.isEmpty {
-                            VStack {
-                                Spacer()
-                                Text("작성된 내용이 없습니다.")
+            VStack {
+                if todoVM.todos.isEmpty {
+                    VStack {
+                        Spacer()
+                        Text("작성된 내용이 없습니다.")
+                            .foregroundStyle(Color.gray)
+                        Spacer()
+                    }
+                    .frame(maxWidth: .infinity, alignment: .center)
+                }
+                else {
+                    List(todoVM.todos) { todo in
+                        NavigationLink(destination: PostDetailView(todo: todo).environmentObject(todoVM)) {
+                            VStack(alignment: .leading, spacing: 5) {
+                                Text(todo.title)
+                                    .font(.headline)
+                                    .lineLimit(1)
+                                Text(todo.content)
+                                    .font(.subheadline)
                                     .foregroundStyle(Color.gray)
-                                Spacer()
+                                    .lineLimit(2)
                             }
-                            .frame(height: geometry.size.height)
-                            .frame(maxWidth: .infinity, alignment: .center)
+                            .padding(.vertical, 5)
                         }
-                        else {
-                            ForEach(todoVM.todos.filter { searchText.isEmpty ||
-                                $0.title.localizedCaseInsensitiveContains(searchText) ||
-                                $0.content.localizedCaseInsensitiveContains(searchText) }, id: \.id) { task in
-//                                NavigationLink(destination: PostDetailView(task: task).environmentObject(firebaseVM)) {
-//
-//                                }
+                        .swipeActions(edge: .trailing) {
+                            Button(role: .destructive, action: {
+                                todoVM.todos.removeAll { $0.id == todo.id }
+                            }) {
+                                Image(systemName: "trash")
                             }
                         }
                     }
+                    .listStyle(.plain)
                 }
-                .navigationTitle(todoVM.kind.localizedName)
-                .fullScreenCover(isPresented: $showIssueFullScreen) {
-                    PostEditorView(title: "새 \(todoVM.kind.localizedName)")
-                        .environmentObject(todoVM)
-                }
-                .toolbar {
-                    ToolbarItemGroup(placement: .topBarTrailing) {
-                        Menu(content: {
-                            Section {
-                                Button(action: {
-                                    
-                                }) {
-                                    Text("정렬: 최신")
-                                }
-                                Button(action: {
-                                    
-                                }) {
-                                    Text("상위: 전체")
-                                }
-                                Button(action: {
-                                    
-                                }) {
-                                    Text("상위: 어제")
-                                }
-                                Button(action: {
-                                    
-                                }) {
-                                    Text("상위: 지난주")
-                                }
-                                Button(action: {
-                                    
-                                }) {
-                                    Text("상위: 지난달")
-                                }
-                                Button(action: {
-                                    
-                                }) {
-                                    Text("상위: 작년")
-                                }
-                            } header: {
-                                Text("필터 옵션")
-                            }
-                        }, label: {
-                            Image(systemName: "ellipsis")
-                        })
-                        Button(action: {
-                            showIssueFullScreen = true
-                        }) {
-                            Image(systemName: "plus")
-                        }
-                    }
-                }
-                .toolbarBackground(.visible, for: .navigationBar)
-                .searchable(
-                    text: $searchText,
-                    placement: .navigationBarDrawer(displayMode: .always),
-                    prompt: "\(todoVM.kind.localizedName) 검색"
-                )
             }
-        }
-        .onAppear {
-            Task {
-                await todoVM.requestTodoList()
+            .navigationTitle(todoVM.kind.localizedName)
+            .fullScreenCover(isPresented: $showIssueFullScreen) {
+                PostEditorView(title: "새 \(todoVM.kind.localizedName)")
+                    .environmentObject(todoVM)
+            }
+            .toolbar {
+                ToolbarItemGroup(placement: .topBarTrailing) {
+                    Menu(content: {
+                        Section {
+                            Button(action: {
+                                
+                            }) {
+                                Text("정렬: 최신")
+                            }
+                            Button(action: {
+                                
+                            }) {
+                                Text("상위: 전체")
+                            }
+                            Button(action: {
+                                
+                            }) {
+                                Text("상위: 어제")
+                            }
+                            Button(action: {
+                                
+                            }) {
+                                Text("상위: 지난주")
+                            }
+                            Button(action: {
+                                
+                            }) {
+                                Text("상위: 지난달")
+                            }
+                            Button(action: {
+                                
+                            }) {
+                                Text("상위: 작년")
+                            }
+                        } header: {
+                            Text("필터 옵션")
+                        }
+                    }, label: {
+                        Image(systemName: "ellipsis")
+                    })
+                    Button(action: {
+                        showIssueFullScreen = true
+                    }) {
+                        Image(systemName: "plus")
+                    }
+                }
+            }
+            .toolbarBackground(.visible, for: .navigationBar)
+            .searchable(
+                text: $todoVM.searchText,
+                placement: .navigationBarDrawer(displayMode: .always),
+                prompt: "\(todoVM.kind.localizedName) 검색"
+            )
+            .searchScopes($todoVM.scope) {
+                ForEach(TodoScope.allCases, id: \.self) { scope in
+                    Text(scope.localizedName).tag(scope)
+                }
+            }
+            .onAppear {
+                Task {
+                    await todoVM.requestTodoList()
+                }
             }
         }
     }
