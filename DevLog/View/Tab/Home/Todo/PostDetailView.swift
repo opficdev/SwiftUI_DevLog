af7217facbeda81fa1a03016acef84c83f5ecae6
opfic <opficdev@gmail.com>
2025-06-13 17:16:31 +0900

    feat: 작성된 TODO의 내용을 보는 뷰 추가

---
+++ DevLog/View/Tab/Home/Todo/PostDetailView.swift
@@ -0,0 +1,58 @@
+//
+//  PostDetailView.swift
+//  DevLog
+//
+//  Created by opfic on 6/12/25.
+//
+
+import SwiftUI
+
+struct PostDetailView: View {
+    @EnvironmentObject private var todoVM: TodoViewModel
+    @State private var todo: Todo
+    
+    init(todo: Todo) {
+        self._todo = State(initialValue: todo)
+    }
+    
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading, spacing: 10) {
+                Text(todo.title)
+                    .font(.title3)
+                    .padding(.horizontal)
+                if let date = todo.dueDate {
+                    Divider()
+                    Text(date.formatted(date: .long, time: .omitted))
+                        .background(
+                            RoundedRectangle(cornerRadius: 8)
+                                .fill(Color.gray.opacity(0.2))
+                        )
+                }
+                Divider()
+                HStack {
+                    Text("태그")
+                    Divider()
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack {
+                            ForEach(todo.tags, id: \.self) { tag in
+                                HStack {
+                                    Text(tag)
+                                }
+                                .padding(.horizontal, 8)
+                                .background(
+                                    Capsule()
+                                        .stroke(Color.gray, lineWidth: 1)
+                                )
+                            }
+                        }
+                    }
+                }
+                .padding(.horizontal)
+                Divider()
+                Text(todo.content)
+                    .padding(.horizontal)
+            }
+        }
+    }
+}
