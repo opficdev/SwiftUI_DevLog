e7bf61f056ffdc3f049dc52e218501ee756ef4a4
opfic <opficdev@gmail.com>
2025-06-19 22:14:06 +0900

    feat: .sheet의 title 및 dismiss() 가 가능하도록 추가

--- DevLog/View/Tab/Home/Todo/TodoManageView.swift
+++ DevLog/View/Tab/Home/Todo/TodoManageView.swift
@@ -8,7 +8,8 @@
 import SwiftUI
 
 struct TodoManageView: View {
-    @Binding var todoKinds: [TodoKind]
+    @EnvironmentObject var homeVM: HomeViewModel
+    @Environment(\.dismiss) private var dismiss
     
     var body: some View {
         List {
@@ -18,8 +19,18 @@ struct TodoManageView: View {
                 }
                
             }
-            .onMove { (source: IndexSet, destination: Int) in
-                todoKinds.move(fromOffsets: source, toOffset: destination)
+            .environment(\.editMode, .constant(EditMode.active))
+            .navigationTitle("TODO 편집")
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarBackButtonHidden()
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action : {
+                        dismiss()
+                    }) {
+                        Text("완료")
+                    }
+                }
             }
         }
         .environment(\.editMode, .constant(EditMode.active))    //  편집 모드 활성화 (row 우측에 line.3.horizontal 추가됨)
