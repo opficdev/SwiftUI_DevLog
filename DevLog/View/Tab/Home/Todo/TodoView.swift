cfabb5f1d1771ccb44ba46272630bbf3c2090a8c
opfic <opficdev@gmail.com>
2025-06-22 20:25:46 +0900

    feat: 좌측에서 swipe하면 중요 표시를 할 수 있도록 추가

--- DevLog/View/Tab/Home/Todo/TodoView.swift
+++ DevLog/View/Tab/Home/Todo/TodoView.swift
@@ -28,24 +28,34 @@ struct TodoView: View {
                     .frame(maxWidth: .infinity, alignment: .center)
                 }
                 else {
-                    List(todoVM.filteredTodos) { todo in
-                        NavigationLink(destination: TodoDetailView(todo: todo).environmentObject(todoVM)) {
-                            VStack(alignment: .leading, spacing: 5) {
-                                Text(todo.title)
-                                    .font(.headline)
-                                    .lineLimit(1)
-                                Text(todo.content)
-                                    .font(.subheadline)
-                                    .foregroundStyle(Color.gray)
-                                    .lineLimit(2)
+                    List {
+                        let todos = todoVM.filteredTodos
+                        ForEach(Array(zip(todos.indices, todos)), id: \.1.id) { index, todo in
+                            NavigationLink(destination: TodoDetailView(todo: todo).environmentObject(todoVM)) {
+                                VStack(alignment: .leading, spacing: 5) {
+                                    Text(todo.title)
+                                        .font(.headline)
+                                        .lineLimit(1)
+                                    Text(todo.content)
+                                        .font(.subheadline)
+                                        .foregroundStyle(Color.gray)
+                                        .lineLimit(2)
+                                }
+                                .padding(.vertical, 5)
                             }
-                            .padding(.vertical, 5)
-                        }
-                        .swipeActions(edge: .trailing) {
-                            Button(role: .destructive, action: {
-                                todoVM.filteredTodos.removeAll { $0.id == todo.id }
-                            }) {
-                                Image(systemName: "trash")
+                            .swipeActions(edge: .leading) {
+                                Button(action: {
+                                    todoVM.filteredTodos[index].isPinned.toggle()
+                                }) {
+                                    Image(systemName: "star\(todo.isPinned ? ".fill" : "")")
+                                }
+                            }
+                            .swipeActions(edge: .trailing) {
+                                Button(role: .destructive, action: {
+                                    todoVM.filteredTodos.removeAll { $0.id == todo.id }
+                                }) {
+                                    Image(systemName: "trash")
+                                }
                             }
                         }
                     }
