5349e95fd3dee647673e6a4ebe27f83f575190f5
opfic <opficdev@gmail.com>
2025-06-28 20:33:24 +0900

    feat: Firestore에 저장할 푸시알람 모델

---
+++ DevLog/Model/PushNotification.swift
@@ -0,0 +1,17 @@
+//
+//  PushNotification.swift
+//  DevLog
+//
+//  Created by opfic on 6/28/25.
+//
+
+import Foundation
+import FirebaseFirestore
+
+struct PushNotification: Codable, Identifiable {
+    @DocumentID var id: String?
+    var title: String
+    var content: String
+    var kind: NotificationKind
+    var receivedDate: Date
+}
