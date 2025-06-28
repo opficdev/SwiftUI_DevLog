bf09517011fee1f5e57373fcd3463aa78948b321
opfic <opficdev@gmail.com>
2025-06-28 20:32:58 +0900

    feat: 푸시알람의 종류

---
+++ DevLog/Enum/NotificationKind.swift
@@ -0,0 +1,15 @@
+//
+//  NotificationKind.swift
+//  DevLog
+//
+//  Created by opfic on 6/28/25.
+//
+
+import Foundation
+
+enum NotificationKind: String, Codable {
+    case info
+    case warning
+    case success
+    case error
+}
