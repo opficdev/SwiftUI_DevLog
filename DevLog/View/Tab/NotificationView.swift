c3f2ccb267cc27760bac74d82873d625656b760e
opfic <opficdev@gmail.com>
2025-07-07 20:19:00 +0900

    feat: 그동안 받은 알람 내역을 확인하는 뷰

--- DevLog/View/Tab/NotificationView.swift
+++ DevLog/View/Tab/NotificationView.swift
@@ -8,11 +8,29 @@
 import SwiftUI
 
 struct NotificationView: View {
+    @StateObject private var notiVM: NotificationViewModel
+    
+    init(notiVM: NotificationViewModel) {
+        self._notiVM = StateObject(wrappedValue: notiVM)
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            List(notiVM.notifications) { noti in
+                
+                
+            }
+            .listStyle(.plain)
+            .navigationTitle("알림")
+            .alert("", isPresented: $notiVM.showAlert) {
+                Button("확인", role: .cancel) { }
+            } message: {
+                Text(notiVM.alertMsg)
+            }
+        }
     }
 }
 
 #Preview {
-    NotificationView()
+    NotificationView(notiVM: AppContainer.shared.notiVM)
 }
