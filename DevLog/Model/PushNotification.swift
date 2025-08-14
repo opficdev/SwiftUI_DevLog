0ec7a4c078de0064bd39c8e5d22bfac9d16a4ec2
opfic <opficdev@gmail.com>
2025-08-14 11:35:45 +0900

    refactor: Rename PushNotification to ReceivedNotification
    Renamed the PushNotification struct to ReceivedNotification and made isRead mutable. Added assignment of id in the initializer and included todoId in the toDictionary method for completeness.

--- DevLog/Model/PushNotification.swift
+++ DevLog/Model/PushNotification.swift
@@ -8,15 +8,16 @@
 import Foundation
 import FirebaseFirestore
 
-struct PushNotification: Codable, Identifiable {
+struct ReceivedNotification: Codable, Identifiable {
     @DocumentID var id: String?
     let title: String   //  알림 제목
     let body: String    //  알림 내용
     let receivedDate: Date  //  알림 수신 날짜
-    let isRead: Bool    //  알림 읽음 여부
+    var isRead: Bool    //  알림 읽음 여부
     let todoId: String //  Todo ID
     
     init(from: QueryDocumentSnapshot) {
+        self.id = from.documentID
         self.title = from["title"] as? String ?? ""
         self.body = from["body"] as? String ?? ""
         self.receivedDate = (from["receivedDate"] as? Timestamp)?.dateValue() ?? Date()
@@ -38,7 +39,8 @@ struct PushNotification: Codable, Identifiable {
             "title": title,
             "body": body,
             "receivedDate": Timestamp(date: receivedDate),
-            "isRead": isRead
+            "isRead": isRead,
+            "todoId": todoId
         ]
     }
 }
