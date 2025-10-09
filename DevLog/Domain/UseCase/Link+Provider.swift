6780fa113af2857a35b162cba097824445abb1ba
opficdev <opficdev@gmail.com>
2025-10-09 22:56:21 +0900

    refactor: struct -> class

--- DevLog/Domain/UseCase/Link+Provider.swift
+++ DevLog/Domain/UseCase/Link+Provider.swift
@@ -7,13 +7,13 @@
 
 import Foundation
 
-struct LinkProvider {
+final class LinkProvider {
     private let repo: AuthRepository
     init(repo: AuthRepository) { self.repo = repo }
     func link(_ provider: AuthProvider) async throws { try await repo.link(provider: provider) }
 }
 
-struct UnlinkProvider {
+final class UnlinkProvider {
     private let repo: AuthRepository
     init(repo: AuthRepository) { self.repo = repo }
     func unlink(_ provider: AuthProvider) async throws { try await repo.unlink(provider: provider) }
